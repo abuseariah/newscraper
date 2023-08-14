3bd5c6212cb62c4d3cb0b347aef20178735868a3
abuseariah <aha1999aha@gmail.com>
2023-08-14 09:00:09 +0300

    first commit

---
+++ src/test/java/com/abuseariah/newscraper/NewscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abuseariah.newscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewscraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
